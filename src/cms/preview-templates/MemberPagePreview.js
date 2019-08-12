89f4af70e4982c60ed88582be853690ea0621e0f
fukui nobuhiro <fukui.nobuhiro@fukufuku-game.com>
2019-08-12 19:34:54 +0900

    メンバーページプレビューから不要な情報を削除

--- src/cms/preview-templates/MemberPagePreview.js
+++ src/cms/preview-templates/MemberPagePreview.js
@@ -5,8 +5,6 @@ import { MemberPageTemplate } from '../../templates/member-page'
 const MemberPagePreview = ({ entry, widgetFor }) => (
   <MemberPageTemplate
     content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
   />
 )
